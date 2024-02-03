0ad3c8da16d818d6e12ccd9648c2d056eed5bd26
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-02-03 11:56:18 +0900

    chore: add logs to oncreatetodo function

--- server/full_dart_server/lib/functions.dart
+++ server/full_dart_server/lib/functions.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:functions_framework/functions_framework.dart';
 import 'package:protobuf_helpers_for_functions/protobuf_helpers_for_functions.dart';
 
@@ -5,9 +8,17 @@ import 'config.dart';
 
 @CloudFunction()
 Future<void> oncreatetodo(CloudEvent event, RequestContext context) async {
+  context.logger.debug(jsonEncode(event.toJson()));
+  context.logger.debug(jsonEncode(context.request.headers));
+  context.logger.debug(jsonEncode(context.request.url));
+  context.logger.debug(jsonEncode(context.request.requestedUri));
+  context.logger.debug(jsonEncode(context.request.method));
+  context.logger.debug(jsonEncode(context.request.handlerPath));
+  context.logger.debug(jsonEncode(context.responseHeaders));
   final documentEventData =
       DocumentEventData.fromBuffer(event.data! as List<int>);
   final json = documentEventData.toProto3Json()! as Map<String, dynamic>;
+  stdout.writeln(jsonEncode(json));
   final documentId = ((json['value'] as Map<String, dynamic>)['name'] as String)
       .split('/')
       .last;
