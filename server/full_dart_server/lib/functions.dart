84e164f4f5b9f8d16fed61610f9f78affe2a64e6
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-02-04 16:52:15 +0900

    feat: update logs

--- server/full_dart_server/lib/functions.dart
+++ server/full_dart_server/lib/functions.dart
@@ -14,7 +14,6 @@ Future<void> oncreatetodo(CloudEvent event, RequestContext context) async {
   );
   final documentId = documentCreatedEvent.id;
   final eventType = documentCreatedEvent.eventType;
-  final json = documentCreatedEvent.value.data;
 
   final title = documentCreatedEvent.value.data['title'] as String?;
   final documentSnapshot =
@@ -26,7 +25,7 @@ Future<void> oncreatetodo(CloudEvent event, RequestContext context) async {
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln('json: ${jsonEncode(json)}');
+  stdout.writeln('event.data: ${event.data}');
 }
 
 @CloudFunction()
@@ -37,16 +36,13 @@ Future<void> onupdatetodo(CloudEvent event, RequestContext context) async {
   );
   final documentId = documentUpdatedEvent.id;
   final eventType = documentUpdatedEvent.eventType;
-  final beforeJson = documentUpdatedEvent.oldValue.data;
-  final afterJson = documentUpdatedEvent.value.data;
 
   context.logger.debug('documentId: $documentId');
   context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln('beforeJson: ${jsonEncode(beforeJson)}');
-  stdout.writeln('afterJson: ${jsonEncode(afterJson)}');
+  stdout.writeln('event.data: ${event.data}');
 }
 
 @CloudFunction()
@@ -57,14 +53,13 @@ Future<void> ondeletetodo(CloudEvent event, RequestContext context) async {
   );
   final documentId = documentDeletedEvent.id;
   final eventType = documentDeletedEvent.eventType;
-  final json = documentDeletedEvent.oldValue.data;
 
   context.logger.debug('documentId: $documentId');
   context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln('json: ${jsonEncode(json)}');
+  stdout.writeln('event.data: ${event.data}');
 }
 
 @CloudFunction()
@@ -75,14 +70,11 @@ Future<void> onwritetodo(CloudEvent event, RequestContext context) async {
   );
   final documentId = documentWrittenEvent.id;
   final eventType = documentWrittenEvent.eventType;
-  final beforeJson = documentWrittenEvent.oldValue?.data;
-  final afterJson = documentWrittenEvent.value?.data;
 
   context.logger.debug('documentId: $documentId');
   context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln('beforeJson: ${jsonEncode(beforeJson)}');
-  stdout.writeln('afterJson: ${jsonEncode(afterJson)}');
+  stdout.writeln('event.data: ${event.data}');
 }
