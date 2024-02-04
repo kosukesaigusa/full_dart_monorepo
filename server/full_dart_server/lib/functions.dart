b13e2327e6580a97daecc9aab83f3dad833f3b30
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-02-04 16:36:44 +0900

    feat: define FirestoreTriggeredEvent

--- server/full_dart_server/lib/functions.dart
+++ server/full_dart_server/lib/functions.dart
@@ -2,103 +2,87 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:functions_framework/functions_framework.dart';
-import 'package:protobuf_helpers_for_functions/protobuf_helpers_for_functions.dart';
 
 import 'config.dart';
 import 'utils/parser.dart';
 
 @CloudFunction()
 Future<void> oncreatetodo(CloudEvent event, RequestContext context) async {
-  context.logger.debug('oncreatetodo function is triggered');
-  final createdDocument = CreatedDocument.fromCloudEvent(
+  final documentCreatedEvent = DocumentCreatedEvent.fromCloudEvent(
     firestore: firestore,
     event: event,
   );
-  final documentId = documentIdFromCloudEvent(event);
-  final eventType = createdDocument.eventType;
+  final documentId = documentCreatedEvent.id;
+  final eventType = documentCreatedEvent.eventType;
+  final json = documentCreatedEvent.value.data;
 
-  final documentEventData =
-      DocumentEventData.fromBuffer(event.data! as List<int>);
-  final json = documentEventData.toProto3Json()! as Map<String, dynamic>;
-  stdout.writeln('json: ${jsonEncode(json)}');
-
-  final title = createdDocument.data['title'] as String?;
+  final title = documentCreatedEvent.value.data['title'] as String?;
   final documentSnapshot =
       await firestore.collection('todos').doc(documentId).get();
   await documentSnapshot.ref.update({'title': '$title from server!'});
+
+  context.logger.debug('documentId: $documentId');
   context.logger.debug('eventType: $eventType');
-  context.logger.debug('subject: ${subjectFromCloudEvent(event)}');
-  context.logger.debug('event.data: ${event.data}');
-  stdout.writeln('event.toJson(): ${jsonEncode(event.toJson())}');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln(
-    'context.responseHeaders: ${jsonEncode(context.responseHeaders)}',
-  );
-  context.logger
-      .debug('context.request.method: ${jsonEncode(context.request.method)}');
+  stdout.writeln('json: ${jsonEncode(json)}');
 }
 
 @CloudFunction()
 Future<void> onupdatetodo(CloudEvent event, RequestContext context) async {
-  context.logger.debug('onupdatetodo function is triggered');
-  final documentEventData =
-      DocumentEventData.fromBuffer(event.data! as List<int>);
-  final json = documentEventData.toProto3Json()! as Map<String, dynamic>;
-  stdout.writeln('json: ${jsonEncode(json)}');
+  final documentUpdatedEvent = DocumentUpdatedEvent.fromCloudEvent(
+    firestore: firestore,
+    event: event,
+  );
+  final documentId = documentUpdatedEvent.id;
+  final eventType = documentUpdatedEvent.eventType;
+  final beforeJson = documentUpdatedEvent.oldValue.data;
+  final afterJson = documentUpdatedEvent.value.data;
 
-  context.logger.debug('subject: ${subjectFromCloudEvent(event)}');
-  context.logger.debug('event.data: ${event.data}');
-  stdout.writeln('event.toJson(): ${jsonEncode(event.toJson())}');
+  context.logger.debug('documentId: $documentId');
+  context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln(
-    'context.responseHeaders: ${jsonEncode(context.responseHeaders)}',
-  );
-  context.logger
-      .debug('context.request.method: ${jsonEncode(context.request.method)}');
+  stdout.writeln('beforeJson: ${jsonEncode(beforeJson)}');
+  stdout.writeln('afterJson: ${jsonEncode(afterJson)}');
 }
 
 @CloudFunction()
 Future<void> ondeletetodo(CloudEvent event, RequestContext context) async {
-  context.logger.debug('ondeletetodo function is triggered');
-  final documentEventData =
-      DocumentEventData.fromBuffer(event.data! as List<int>);
-  final json = documentEventData.toProto3Json()! as Map<String, dynamic>;
-  stdout.writeln('json: ${jsonEncode(json)}');
+  final documentDeletedEvent = DocumentDeletedEvent.fromCloudEvent(
+    firestore: firestore,
+    event: event,
+  );
+  final documentId = documentDeletedEvent.id;
+  final eventType = documentDeletedEvent.eventType;
+  final json = documentDeletedEvent.oldValue.data;
 
-  context.logger.debug('subject: ${subjectFromCloudEvent(event)}');
-  context.logger.debug('event.data: ${event.data}');
-  stdout.writeln('event.toJson(): ${jsonEncode(event.toJson())}');
+  context.logger.debug('documentId: $documentId');
+  context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln(
-    'context.responseHeaders: ${jsonEncode(context.responseHeaders)}',
-  );
-  context.logger
-      .debug('context.request.method: ${jsonEncode(context.request.method)}');
+  stdout.writeln('json: ${jsonEncode(json)}');
 }
 
 @CloudFunction()
 Future<void> onwritetodo(CloudEvent event, RequestContext context) async {
-  context.logger.debug('onwritetodo function is triggered');
-  final documentEventData =
-      DocumentEventData.fromBuffer(event.data! as List<int>);
-  final json = documentEventData.toProto3Json()! as Map<String, dynamic>;
-  stdout.writeln('json: ${jsonEncode(json)}');
+  final documentWrittenEvent = DocumentWrittenEvent.fromCloudEvent(
+    firestore: firestore,
+    event: event,
+  );
+  final documentId = documentWrittenEvent.id;
+  final eventType = documentWrittenEvent.eventType;
+  final beforeJson = documentWrittenEvent.oldValue?.data;
+  final afterJson = documentWrittenEvent.value?.data;
 
-  context.logger.debug('subject: ${subjectFromCloudEvent(event)}');
-  context.logger.debug('event.data: ${event.data}');
-  stdout.writeln('event.toJson(): ${jsonEncode(event.toJson())}');
+  context.logger.debug('documentId: $documentId');
+  context.logger.debug('eventType: $eventType');
   stdout.writeln(
     'context.request.headers ${jsonEncode(context.request.headers)}',
   );
-  stdout.writeln(
-    'context.responseHeaders: ${jsonEncode(context.responseHeaders)}',
-  );
-  context.logger
-      .debug('context.request.method: ${jsonEncode(context.request.method)}');
+  stdout.writeln('beforeJson: ${jsonEncode(beforeJson)}');
+  stdout.writeln('afterJson: ${jsonEncode(afterJson)}');
 }
--- server/full_dart_server/lib/utils/parser.dart
+++ server/full_dart_server/lib/utils/parser.dart
@@ -11,63 +11,190 @@ enum FirestoreDocumentEventType {
   ;
 
   factory FirestoreDocumentEventType.fromCloudEvent(CloudEvent event) {
-    final type = eventType(event);
-    if (type == 'google.cloud.firestore.document.v1.created') {
-      return FirestoreDocumentEventType.v1Created;
-    } else if (type == 'google.cloud.firestore.document.v1.updated') {
-      return FirestoreDocumentEventType.v1Updated;
-    } else if (type == 'google.cloud.firestore.document.v1.deleted') {
-      return FirestoreDocumentEventType.v1Deleted;
-    } else if (type == 'google.cloud.firestore.document.v1.written') {
-      return FirestoreDocumentEventType.v1Written;
-    }
-
-    throw ArgumentError.value(
-      type,
-      'event',
-      'Cannot convert CloudEvent type to FirestoreDocumentEventType',
-    );
+    final type = event.type;
+    return switch (type) {
+      'google.cloud.firestore.document.v1.created' =>
+        FirestoreDocumentEventType.v1Created,
+      'google.cloud.firestore.document.v1.updated' =>
+        FirestoreDocumentEventType.v1Updated,
+      'google.cloud.firestore.document.v1.deleted' =>
+        FirestoreDocumentEventType.v1Deleted,
+      'google.cloud.firestore.document.v1.written' =>
+        FirestoreDocumentEventType.v1Written,
+      _ => throw ArgumentError.value(
+          type,
+          'event',
+          'Cannot convert CloudEvent type to FirestoreDocumentEventType',
+        ),
+    };
   }
 }
 
-class CreatedDocument {
-  CreatedDocument._({
+sealed class FirestoreTriggeredEvent {
+  const FirestoreTriggeredEvent({
     required this.id,
     required this.path,
-    required this.data,
     required this.eventType,
-    required this.createTime,
-    required this.updateTime,
   });
-
-  factory CreatedDocument.fromCloudEvent({
-    required admin.Firestore firestore,
-    required CloudEvent event,
-  }) {
-    final documentEventData =
-        proto.DocumentEventData.fromBuffer(event.data! as List<int>);
-    return CreatedDocument._(
-      id: documentIdFromCloudEvent(event),
-      path: subjectPathFromCloudEvent(event),
-      eventType: FirestoreDocumentEventType.fromCloudEvent(event),
-      data: {
-        for (final field in documentEventData.value.fields.entries)
-          field.key: _decodeValue(firestore: firestore, value: field.value),
-      },
-      createTime: admin.Timestamp.fromDate(
-        documentEventData.value.createTime.toDateTime(),
-      ),
-      updateTime: admin.Timestamp.fromDate(
-        documentEventData.value.updateTime.toDateTime(),
-      ),
-    );
-  }
 
   final String id;
 
   final String path;
 
   final FirestoreDocumentEventType eventType;
+}
+
+class DocumentCreatedEvent extends FirestoreTriggeredEvent {
+  const DocumentCreatedEvent._({
+    required super.id,
+    required super.path,
+    required super.eventType,
+    required this.value,
+  });
+
+  factory DocumentCreatedEvent.fromCloudEvent({
+    required admin.Firestore firestore,
+    required CloudEvent event,
+  }) {
+    final documentEventData =
+        proto.DocumentEventData.fromBuffer(event.data! as List<int>);
+    return DocumentCreatedEvent._(
+      id: _documentIdFromCloudEvent(event),
+      path: _subjectPathFromCloudEvent(event),
+      eventType: FirestoreDocumentEventType.v1Created,
+      value: EventTriggeredDocument.fromDocument(
+        firestore: firestore,
+        document: documentEventData.value,
+      ),
+    );
+  }
+
+  final EventTriggeredDocument value;
+}
+
+class DocumentUpdatedEvent extends FirestoreTriggeredEvent {
+  const DocumentUpdatedEvent._({
+    required super.id,
+    required super.path,
+    required super.eventType,
+    required this.value,
+    required this.oldValue,
+  });
+
+  factory DocumentUpdatedEvent.fromCloudEvent({
+    required admin.Firestore firestore,
+    required CloudEvent event,
+  }) {
+    final documentEventData =
+        proto.DocumentEventData.fromBuffer(event.data! as List<int>);
+    return DocumentUpdatedEvent._(
+      id: _documentIdFromCloudEvent(event),
+      path: _subjectPathFromCloudEvent(event),
+      eventType: FirestoreDocumentEventType.v1Updated,
+      value: EventTriggeredDocument.fromDocument(
+        firestore: firestore,
+        document: documentEventData.value,
+      ),
+      oldValue: EventTriggeredDocument.fromDocument(
+        firestore: firestore,
+        document: documentEventData.oldValue,
+      ),
+    );
+  }
+
+  final EventTriggeredDocument value;
+
+  final EventTriggeredDocument oldValue;
+}
+
+class DocumentDeletedEvent extends FirestoreTriggeredEvent {
+  const DocumentDeletedEvent._({
+    required super.id,
+    required super.path,
+    required super.eventType,
+    required this.oldValue,
+  });
+
+  factory DocumentDeletedEvent.fromCloudEvent({
+    required admin.Firestore firestore,
+    required CloudEvent event,
+  }) {
+    final documentEventData =
+        proto.DocumentEventData.fromBuffer(event.data! as List<int>);
+    return DocumentDeletedEvent._(
+      id: _documentIdFromCloudEvent(event),
+      path: _subjectPathFromCloudEvent(event),
+      eventType: FirestoreDocumentEventType.v1Deleted,
+      oldValue: EventTriggeredDocument.fromDocument(
+        firestore: firestore,
+        document: documentEventData.oldValue,
+      ),
+    );
+  }
+
+  final EventTriggeredDocument oldValue;
+}
+
+class DocumentWrittenEvent extends FirestoreTriggeredEvent {
+  const DocumentWrittenEvent._({
+    required super.id,
+    required super.path,
+    required super.eventType,
+    required this.value,
+    required this.oldValue,
+  });
+
+  factory DocumentWrittenEvent.fromCloudEvent({
+    required admin.Firestore firestore,
+    required CloudEvent event,
+  }) {
+    final documentEventData =
+        proto.DocumentEventData.fromBuffer(event.data! as List<int>);
+    final value = documentEventData.value;
+    final oldValue = documentEventData.oldValue;
+    return DocumentWrittenEvent._(
+      id: _documentIdFromCloudEvent(event),
+      path: _subjectPathFromCloudEvent(event),
+      eventType: FirestoreDocumentEventType.v1Written,
+      value: value.hasName()
+          ? EventTriggeredDocument.fromDocument(
+              firestore: firestore,
+              document: value,
+            )
+          : null,
+      oldValue: oldValue.hasName()
+          ? EventTriggeredDocument.fromDocument(
+              firestore: firestore,
+              document: oldValue,
+            )
+          : null,
+    );
+  }
+
+  final EventTriggeredDocument? value;
+
+  final EventTriggeredDocument? oldValue;
+}
+
+class EventTriggeredDocument {
+  EventTriggeredDocument._({
+    required this.data,
+    required this.createTime,
+    required this.updateTime,
+  });
+
+  factory EventTriggeredDocument.fromDocument({
+    required admin.Firestore firestore,
+    required proto.Document document,
+  }) =>
+      EventTriggeredDocument._(
+        data: {
+          for (final field in document.fields.entries)
+            field.key: _decodeValue(firestore: firestore, value: field.value),
+        },
+        createTime: admin.Timestamp.fromDate(document.createTime.toDateTime()),
+        updateTime: admin.Timestamp.fromDate(document.updateTime.toDateTime()),
+      );
 
   final Map<String, Object?> data;
 
@@ -76,20 +203,17 @@ class CreatedDocument {
   final admin.Timestamp updateTime;
 }
 
-// google.cloud.firestore.document.v1.updated
-String eventType(CloudEvent event) => event.type;
-
 // documents/users/ghXNtePIFmdDOBH3iEMH
-String subjectFromCloudEvent(CloudEvent event) =>
+String _subjectFromCloudEvent(CloudEvent event) =>
     event.toJson()['subject'] as String;
 
 // users/ghXNtePIFmdDOBH3iEMH
-String subjectPathFromCloudEvent(CloudEvent event) =>
-    subjectFromCloudEvent(event).replaceFirst('documents/', '');
+String _subjectPathFromCloudEvent(CloudEvent event) =>
+    _subjectFromCloudEvent(event).replaceFirst('documents/', '');
 
 // ghXNtePIFmdDOBH3iEMH
-String documentIdFromCloudEvent(CloudEvent event) =>
-    subjectPathFromCloudEvent(event).split('/').last;
+String _documentIdFromCloudEvent(CloudEvent event) =>
+    _subjectPathFromCloudEvent(event).split('/').last;
 
 Object? _decodeValue({
   required admin.Firestore firestore,
