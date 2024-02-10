5ed7def4fdc710d42754c1df0c83bb499cc91d46
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-02-10 23:38:59 +0900

    feat: update entry point

--- client/full_dart_client/lib/main.dart
+++ client/full_dart_client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
@@ -10,11 +11,14 @@ import 'todo_list.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await LineSDK.instance.setup('2003353308');
+  if (!kIsWeb) {
+    await LineSDK.instance.setup('2003353308');
+  }
 
   runApp(
     ProviderScope(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: const TodoListPage(),
         scrollBehavior: const MaterialScrollBehavior().copyWith(
           dragDevices: {
